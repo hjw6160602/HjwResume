242ee820cfd26a83fc6e86b6675f9e31256d3928
hejiawei <hejiawei@qiyi.com>
2022-08-21 17:35:39 +0800

    .add: 打通数据库网站显示

--- src/main/java/com/wj/hr/dao/WebsiteDao.java
+++ src/main/java/com/wj/hr/dao/WebsiteDao.java
@@ -1,4 +1,62 @@
 package com.wj.hr.dao;
 
+import com.alibaba.druid.pool.DruidDataSourceFactory;
+import com.wj.hr.bean.Website;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
+import java.util.List;
+import java.util.Properties;
+
 public class WebsiteDao {
+    private static JdbcTemplate tpl;
+    static {
+        try {
+            // 获取连接池
+            Properties properties = new Properties();
+            properties.load(WebsiteDao.class.getClassLoader().getResourceAsStream("druid.properties"));
+            DataSource ds = DruidDataSourceFactory.createDataSource(properties);
+            // 创建tpl
+            tpl = new JdbcTemplate(ds);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    /**
+     * 删除记录
+     */
+    public boolean remove(Integer id) {
+        return false;
+    }
+
+    /**
+     * 添加或更新
+     */
+    public boolean save(Website website) {
+        return false;
+    }
+
+    /**
+     * 获取单个对象
+     */
+    public Website get(Integer id) {
+        return null;
+    }
+
+    /**
+     * 获取多个对象ç
+     */
+    public List<Website> list () {
+        String sql = "SELECT id, created_time, footer FROM website";
+        return tpl.query(sql, new BeanPropertyRowMapper<Website>(Website.class));
+    }
+
+    /**
+     * 获取统计值
+     */
+    public int count() {
+        return 0;
+    }
+
 }
--- src/main/java/com/wj/hr/servlet/WebsiteServlet.java
+++ src/main/java/com/wj/hr/servlet/WebsiteServlet.java
@@ -1,13 +1,32 @@
 package com.wj.hr.servlet;
 
+import com.wj.hr.bean.Website;
+import com.wj.hr.dao.WebsiteDao;
+
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.List;
 
 @WebServlet("/website/*")
 public class WebsiteServlet extends BaseServlet {
-    public void admin(HttpServletRequest request, HttpServletResponse response) throws Exception {
-        System.out.println("website - admin777");
+    private WebsiteDao dao = new WebsiteDao();
 
+    public void admin(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        List<Website> websites = dao.list();
+        Website website = (websites != null && !websites.isEmpty()) ? websites.get(0) : null;
+        request.setAttribute("website", website);
+        System.out.println(website.getFooter());
+        // 转发
+        request.getRequestDispatcher("/page/admin/website.jsp").forward(request, response);
+    }
+
+    public void save(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        List<Website> websites = dao.list();
+        Website website = (websites != null && !websites.isEmpty()) ? websites.get(0) : null;
+
+        request.setAttribute("websites", website);
+        // 转发
+        request.getRequestDispatcher("/page/admin/website.jsp").forward(request, response);
     }
 }
